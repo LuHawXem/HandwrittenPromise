1f4fc61593d19f1905a1134b8a7247af3bfa54df
草帽 <1906100003@e.gzhu.edu.cn>
2022-03-23 12:05:08 +0800

    格式化代码

--- SelfPromise.js
+++ SelfPromise.js
@@ -1,25 +1,25 @@
 const PromiseState = {
-    kPending: "pending",
-    kFulfilled: "resolved",
-    kRejected: "rejected",
-}
+  kPending: "pending",
+  kFulfilled: "resolved",
+  kRejected: "rejected",
+};
 
 /**
  * @class PromiseReaction
  * @constructor
  */
 class PromiseReaction {
-    /**
-     * @param {Function} onResolveCallback Promise状态转换为fulfilled时执行的回调函数
-     * @param {Function} onRejectCallback Promise状态转换为rejected时执行的回调函数
-     * @param {PromiseReaction} next 指向下一个结点的指针
-     */
-    constructor(onResolveCallback, onRejectCallback, next) {
-        if(!new.target) throw new TypeError(`${new.target} is not a reaction`);
-        this.onResolveCallback = onResolveCallback;
-        this.onRejectCallback = onRejectCallback;
-        this.next = next;
-    }
+  /**
+   * @param {Function} onResolveCallback Promise状态转换为fulfilled时执行的回调函数
+   * @param {Function} onRejectCallback Promise状态转换为rejected时执行的回调函数
+   * @param {PromiseReaction} next 指向下一个结点的指针
+   */
+  constructor(onResolveCallback, onRejectCallback, next) {
+    if (!new.target) throw new TypeError(`${new.target} is not a reaction`);
+    this.onResolveCallback = onResolveCallback;
+    this.onRejectCallback = onRejectCallback;
+    this.next = next;
+  }
 }
 
 /**
@@ -30,45 +30,48 @@ class PromiseReaction {
  * @returns {void}
  */
 function resolvePromise(promise, x, resolve, reject) {
-    if(promise === x) return reject(new TypeError("cylce chaining"));
-    if(x !== null && (typeof x === "function" || typeof x === "object")) {
-        let isCalled = false;
-        try {
-            let then = x.then;
-            if(typeof then === "function") {
-                /* thenable要在新的微任务中调用,于何处规定不明 */
-                queueMicrotask(() => {
-                    try {
-                        then.call(x, r => {
-                            if(isCalled) return;
-                            isCalled = true;
-                            resolvePromise(promise, r, resolve, reject);
-                        }, e => {
-                            if(isCalled) return;
-                            isCalled = true;
-                            reject(e);
-                        })
-                    } catch(e) {
-                        if(isCalled) return;
-                        isCalled = true;
-                        reject(e);
-                    }
-                })
-            }
-            else {
-                if(isCalled) return;
+  if (promise === x) return reject(new TypeError("cylce chaining"));
+
+  if (x !== null && (typeof x === "function" || typeof x === "object")) {
+    let isCalled = false;
+    try {
+      let then = x.then;
+      if (typeof then === "function") {
+        /* thenable要在新的微任务中调用,于何处规定不明 */
+        queueMicrotask(() => {
+          try {
+            then.call(
+              x,
+              (r) => {
+                if (isCalled) return;
                 isCalled = true;
-                resolve(x);
-            }
-        } catch(e) {
-            if(isCalled) return;
+                resolvePromise(promise, r, resolve, reject);
+              },
+              (e) => {
+                if (isCalled) return;
+                isCalled = true;
+                reject(e);
+              }
+            );
+          } catch (e) {
+            if (isCalled) return;
             isCalled = true;
             reject(e);
-        }
-    }
-    else {
+          }
+        });
+      } else {
+        if (isCalled) return;
+        isCalled = true;
         resolve(x);
+      }
+    } catch (e) {
+      if (isCalled) return;
+      isCalled = true;
+      reject(e);
     }
+  } else {
+    resolve(x);
+  }
 }
 
 /**
@@ -78,28 +81,29 @@ function resolvePromise(promise, x, resolve, reject) {
  * @returns {void}
  */
 function triggerPromiseReaction(reactions, result, status) {
-    if(status === PromiseState.kPending) return;
-    let reversed = null;
-    let cur = reactions;
-    while(cur) {
-        let current = cur.next;
-        cur.next = reversed;
-        reversed = cur;
-        cur = current;
+  if (status === PromiseState.kPending) return;
+
+  let reversed = null;
+  let cur = reactions;
+  while (cur) {
+    let current = cur.next;
+    cur.next = reversed;
+    reversed = cur;
+    cur = current;
+  }
+
+  cur = reversed;
+  if (status === PromiseState.kFulfilled) {
+    while (cur) {
+      cur.onResolveCallback(result);
+      cur = cur.next;
     }
-    cur = reversed;
-    if(status === PromiseState.kFulfilled) {
-        while(cur) {
-            cur.onResolveCallback(result);
-            cur = cur.next;
-        }
+  } else {
+    while (cur) {
+      cur.onRejectCallback(result);
+      cur = cur.next;
     }
-    else {
-        while(cur) {
-            cur.onRejectCallback(result);
-            cur = cur.next;
-        }
-    }
+  }
 }
 
 /**
@@ -107,152 +111,161 @@ function triggerPromiseReaction(reactions, result, status) {
  * @constructor
  */
 class SelfPromise {
+  /**
+   * @param {function(resolve, reject)} exec
+   */
+  constructor(exec) {
+    if (!new.target) throw new TypeError(`${new.target} is not a promise`);
+    if (typeof exec !== "function") throw new TypeError(`${exec} resolver is not a function`);
+
+    const self = this;
+    self.status = PromiseState.kPending;
+    self.reactions_or_result = null;
+
     /**
-     * @param {function(resolve, reject)} exec
+     * @param {any} value
      */
-    constructor(exec) {
-        if(!new.target) throw new TypeError(`${new.target} is not a promise`);
-        if(typeof exec !== "function") throw new TypeError(`${exec} resolver is not a function`);
-        const self = this;
-        self.status = PromiseState.kPending;
-        self.reactions_or_result = null;
+    function fulfill(value) {
+      self.status = PromiseState.kFulfilled;
+      const reactions = self.reactions_or_result;
+      self.reactions_or_result = value;
+      triggerPromiseReaction(reactions, self.reactions_or_result, self.status);
+    }
 
-        /**
-         * @param {any} value 
-         */
-        function fulfill(value) {
-            self.status = PromiseState.kFulfilled;
-            const reactions = self.reactions_or_result;
-            self.reactions_or_result = value;
-            triggerPromiseReaction(reactions, self.reactions_or_result, self.status);
-        }
+    /**
+     * @param {any} value
+     */
+    function resolve(value) {
+      if (self.status === PromiseState.kPending) {
+        resolvePromise(self, value, fulfill, reject);
+      }
+    }
 
-        /**
-         * @param {any} value 
-         */
-        function resolve(value) {
-            if(self.status === PromiseState.kPending) {
-                resolvePromise(self, value, fulfill, reject);
+    /**
+     * @param {any} reason
+     */
+    function reject(reason) {
+      if (self.status === PromiseState.kPending) {
+        self.status = PromiseState.kRejected;
+        const reactions = self.reactions_or_result;
+        self.reactions_or_result = reason;
+        triggerPromiseReaction(
+          reactions,
+          self.reactions_or_result,
+          self.status
+        );
+      }
+    }
+
+    try {
+      exec(resolve, reject);
+    } catch (e) {
+      reject(e);
+    }
+  }
+
+  /**
+   * @param {any} onFulfilled resolve的处理函数或undefined
+   * @param {any} onRejected reject的处理函数或undefined
+   * @returns {SelfPromise}
+   */
+  then(onFulfilled, onRejected) {
+    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : (x) => x;
+    onRejected = typeof onRejected === "function" ? onRejected: (e) => { throw e; };
+
+    const self = this;
+    let promise = new SelfPromise((resolve, reject) => {
+      switch (self.status) {
+        case PromiseState.kPending:
+          let reaction = new PromiseReaction(
+            () => {
+              queueMicrotask(() => {
+                try {
+                  let x = onFulfilled(self.reactions_or_result);
+                  resolve(x);
+                } catch (e) {
+                  reject(e);
+                }
+              });
+            },
+            () => {
+              queueMicrotask(() => {
+                try {
+                  let x = onRejected(self.reactions_or_result);
+                  resolve(x);
+                } catch (e) {
+                  reject(e);
+                }
+              });
             }
-        }
-
-        /**
-         * @param {any} reason 
-         */
-        function reject(reason) {
-            if(self.status === PromiseState.kPending) {
-                self.status = PromiseState.kRejected;
-                const reactions = self.reactions_or_result;
-                self.reactions_or_result = reason;
-                triggerPromiseReaction(reactions, self.reactions_or_result, self.status);
+          );
+          reaction.next = self.reactions_or_result;
+          self.reactions_or_result = reaction;
+          break;
+        case PromiseState.kFulfilled:
+          queueMicrotask(() => {
+            try {
+              let x = onFulfilled(self.reactions_or_result);
+              resolve(x);
+            } catch (e) {
+              reject(e);
             }
-        }
-
-        try {
-            exec(resolve, reject);
-        } catch(e) {
-            reject(e);
-        }
-    }
-
-    /**
-     * @param {any} onFulfilled resolve的处理函数或undefined
-     * @param {any} onRejected reject的处理函数或undefined
-     * @returns {SelfPromise}
-     */
-    then(onFulfilled, onRejected) {
-        onFulfilled = typeof onFulfilled === "function" ? onFulfilled : x => x;
-        onRejected = typeof onRejected === "function" ? onRejected : e => { throw e; };
-        const self = this;
-        let promise = new SelfPromise((resolve, reject) => {
-            switch(self.status) {
-                case PromiseState.kPending:
-                    let reaction = new PromiseReaction(() => {
-                        queueMicrotask(() => {
-                            try {
-                                let x = onFulfilled(self.reactions_or_result);
-                                resolve(x);
-                            } catch(e) {
-                                reject(e);
-                            }
-                        })
-                    }, () => {
-                        queueMicrotask(() => {
-                            try {
-                                let x = onRejected(self.reactions_or_result);
-                                resolve(x);
-                            } catch(e) {
-                                reject(e);
-                            }
-                        })
-                    });
-                    reaction.next = self.reactions_or_result;
-                    self.reactions_or_result = reaction;
-                    break;
-                case PromiseState.kFulfilled:
-                    queueMicrotask(() => {
-                        try {
-                            let x = onFulfilled(self.reactions_or_result);
-                            resolve(x);
-                        } catch(e) {
-                            reject(e);
-                        }
-                    })
-                    break;
-                case PromiseState.kRejected:
-                    queueMicrotask(() => {
-                        try {
-                            let x = onRejected(self.reactions_or_result);
-                            resolve(x);
-                        } catch(e) {
-                            reject(e);
-                        }
-                    })
-                    break;
+          });
+          break;
+        case PromiseState.kRejected:
+          queueMicrotask(() => {
+            try {
+              let x = onRejected(self.reactions_or_result);
+              resolve(x);
+            } catch (e) {
+              reject(e);
             }
-        })
-        return promise;
-    }
+          });
+          break;
+      }
+    });
+    return promise;
+  }
 
-    /**
-     * @param {any} onRejected 
-     * @returns {SelfPromise}
-     */
-    catch(onRejected) {
-        return this.then(undefined, onRejected);
-    }
+  /**
+   * @param {any} onRejected
+   * @returns {SelfPromise}
+   */
+  catch(onRejected) {
+    return this.then(undefined, onRejected);
+  }
 
-    /**
-     * @param {any} value 
-     * @returns {SelfPromise}
-     */
-    static resolve(value) {
-        if(value instanceof SelfPromise) {
-            return value;
-        }
-        return new SelfPromise((resolve) => {
-            resolve(value);
-        })
+  /**
+   * @param {any} value
+   * @returns {SelfPromise}
+   */
+  static resolve(value) {
+    if (value instanceof SelfPromise) {
+      return value;
     }
+    return new SelfPromise((resolve) => {
+      resolve(value);
+    });
+  }
 
-    /**
-     * @param {any} reason 
-     * @returns {SelfPromise}
-     */
-    static reject(reason) {
-        return new SelfPromise((_, reject) => {
-            reject(reason);
-        })
-    }
+  /**
+   * @param {any} reason
+   * @returns {SelfPromise}
+   */
+  static reject(reason) {
+    return new SelfPromise((_, reject) => {
+      reject(reason);
+    });
+  }
 }
 
-SelfPromise.deferred = function() {
-    let dfd = {};
-    dfd.promise = new SelfPromise((resolve, reject) => {
-        dfd.resolve = resolve;
-        dfd.reject = reject;
-    })
-    return dfd;
-}
+SelfPromise.deferred = function () {
+  let dfd = {};
+  dfd.promise = new SelfPromise((resolve, reject) => {
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+  return dfd;
+};
 
 module.exports = SelfPromise;
